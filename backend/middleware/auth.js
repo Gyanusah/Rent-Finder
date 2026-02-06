db0568d27fdfb902a7b1b5857a8a210b8fe04e32
Gyanendra Shah <shahgyanendra555@gmail.com>
2026-02-06 13:57:44 +0530

    first commit

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,31 +1,3 @@
-// const jwt = require('jsonwebtoken');
-
-// const auth = (req, res, next) => {
-//     try {
-//         const token = req.headers.authorization?.split(' ')[1];
-
-//         if (!token) {
-//             return res.status(401).json({ message: 'No token, authorization denied' });
-//         }
-
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//         req.user = decoded;
-//         next();
-//     } catch (error) {
-//         res.status(401).json({ message: 'Token is not valid' });
-//     }
-// };
-
-// const authorize = (...roles) => (req, res, next) => {
-//     if (!roles.includes(req.user.role)) {
-//         return res.status(403).json({ message: 'Not authorized to access this route' });
-//     }
-//     next();
-// };
-
-// module.exports = { auth, authorize };
-
-
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
