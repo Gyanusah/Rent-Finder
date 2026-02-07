453bde0872f9a014cc7d73d2f5d5898f402b818f
Gyanendra Shah <shahgyanendra555@gmail.com>
2026-02-07 20:00:05 +0530

    first commit

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -3,7 +3,10 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL 
+// // || 'http://localhost:5000/api';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://rent-finder-n4vs.vercel.app/api';
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
